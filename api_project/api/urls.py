e525d437ac1d8d8d7225a00f56d901fa5c970838
Kyra Cnh <mokatel52@gmail.com>
2024-11-24 21:22:49 +0100

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
-from .views import BookListAPIView, BookViewSet
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import BookListAPIView, BookViewSet
+
+# Router for the ViewSet
 router = DefaultRouter()
-router.register(r'books', BookViewSet)
+router.register(r'books', BookViewSet, basename='book')
+
 urlpatterns = [
-    path('books/', BookListAPIView.as_view(), name = 'book-list'),
-    path('', include(router.urls)),
-    path('api-token-auth/', obtain_auth_token, name='api-token-auth'),
+    path('books-list/', BookListAPIView.as_view(), name='book-list'),  # List API view
+    path('', include(router.urls)),  # Routes for the ViewSet
+    path('api-token-auth/', obtain_auth_token, name='api-token-auth'),  # Token auth
 ]
